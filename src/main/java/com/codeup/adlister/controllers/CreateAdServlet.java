4458cdae75e1d20636bd68fdc1564cb2dd10c51f
benjaminhodge <benjamin.andrew.hodge@gmail.com>
2018-04-05 14:53:23 -0500

    password solution

--- src/main/java/com/codeup/adlister/controllers/CreateAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/CreateAdServlet.java
@@ -13,11 +13,21 @@ import java.io.IOException;
 @WebServlet(name = "controllers.CreateAdServlet", urlPatterns = "/ads/create")
 public class CreateAdServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        Check if logged in
+
+        if (request.getSession().getAttribute("user") == null){
+            response.sendRedirect("/login");
+            return;
+        }
+
         request.getRequestDispatcher("/WEB-INF/ads/create.jsp")
             .forward(request, response);
     }
 
+
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
         Ad ad = new Ad(
             1, // for now we'll hardcode the user id
             request.getParameter("title"),
